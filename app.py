b526262ce18069e580afc191a00aa96d45bac478
Nathan LeClaire <nathan.leclaire@gmail.com>
2014-10-05 21:33:53 -0700

    Make Python script work

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import git
-import os
+import sys
 
-url = os.argv[1]
-path = "repos/{0}".format(url.split("/", 3))
+url = sys.argv[1]
+path = "repos/{0}".format(url.split("/", 3)[-1])
 repo = git.repo.base.Repo()
 repo.clone_from(url, path)
