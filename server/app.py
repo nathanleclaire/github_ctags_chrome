3b11601d4bea5188e49069e6717c69c21e161f83
Nathan LeClaire <nathan.leclaire@gmail.com>
2014-10-05 21:56:34 -0700

    Continue making script work well

--- server/app.py
+++ server/app.py
@@ -1,7 +1,9 @@
-import git
-import sys
+from git.repo.base import Repo
+import sys, os
 
 url = sys.argv[1]
 path = "repos/{0}".format(url.split("/", 3)[-1])
-repo = git.repo.base.Repo()
-repo.clone_from(url, path)
+
+if __name__ == '__main__':
+    if not os.path.isdir(path):
+        repo = Repo.clone_from(url, path)
